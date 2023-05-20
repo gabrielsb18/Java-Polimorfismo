88885a9906faf0657a683d2a865bedb0452e4454
gabrielsb18 <gbmodelator@gmail.com>
2023-05-20 18:03:39 -0300

    methodos_abstratos

--- bytebank-herdado/src/ControleBonificacao.java
+++ bytebank-herdado/src/ControleBonificacao.java
@@ -13,6 +13,10 @@ public class ControleBonificacao {
 
 }
 
-//
+//Polimorfismo : "referências de tipos de classes mais genéricas referenciem objetos mais específicos" (alura).
+
+//Obs: O tipo do objeto não pode mudar e sim sua referencia, indo de uma mais especifica pra uma generica, como no nosso exemplo a classe funcionario que aponta para a classe especifica gerente ou editor, assim por diante. É AQUI ONDE ENTRA O POLIMORFISMO!! O polimorfismo permite usar referências mais genéricas para a comunicação com um objeto.
+
+//o uso de referências mais genéricas permite desacoplar um sistema.
 
 // O polimorfismo permite nós a criarmos um metodo generico onde não precisamos mais criar um metodo especifico pra cada tipo de funcionario, no nosso exemplo,  que ainda sim irá chamar o metodo especifico definido em sua respectiva classe.
